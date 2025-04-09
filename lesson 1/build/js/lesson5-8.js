d43777086e3ddb9bb0cfdb0f9b06dac3b9e035ce
SantiagoYpes <santiyezu8@hotmail.com>
2025-04-09 11:58:10 -0500

    Lesson advance 5-7

--- lesson 1/build/js/lesson5-8.js
+++ lesson 1/build/js/lesson5-8.js
@@ -18,4 +18,121 @@ let myOp1 = addOrConcat(2, 2, 'add');
 const img = document.querySelector('img');
 const myImg = document.getElementById('#id');
 const nextImg = document.getElementById('#id');
-myImg.src;
+//myImg.src
+//Classes
+class Coder {
+    constructor(name, music, age, lang = 'Typescript') {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name,
+            this.music = music,
+            this.age = age,
+            this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Dave = new Coder('Santi', 'Electronic', 21, 'JavaSript');
+console.log(Dave.getAge);
+//Extends
+class WebDev extends Coder {
+    constructor(computer, name, music, age, lang = 'Typescript') {
+        super(name, music, age, lang);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I wirte ${this.lang}`;
+    }
+}
+const Mari = new WebDev('HP', 'Mari', 'POP', 23, 'Python');
+class Percusionist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `You are ${action} ${this.instrument}`;
+    }
+}
+const Page = new Percusionist('Ana', 'Tambor');
+console.log(Page.play('Beating'));
+// Static variables and methods
+class Peep {
+    static getCount() {
+        return Peep.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peep.count;
+    }
+}
+Peep.count = 0;
+const John = new Peep('John');
+const Steve = new Peep('Steve');
+const Amy = new Peep('Amy');
+console.log(Steve.id);
+// Setters and getters
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings');
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ['Rock', 'Pop', 'Jazz'];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, 'Metal'];
+console.log(MyBands.data);
+const todaysTransactions = {
+    Pizza: -10,
+    Books: 20,
+    Job: 1000
+};
+console.log(todaysTransactions.Pizza);
+const todayNetTransactions = (transactions) => {
+    let total = 0;
+    for (const transaction in transactions) {
+        total += transactions[transaction];
+    }
+    return total;
+};
+const student = {
+    name: 'John',
+    GPA: 3.5,
+    classes: [1, 2, 3, 4, 5]
+};
+//console.log(student.test)
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map(key => {
+    console.log(student[key]);
+});
+const stundentLog = (student, key) => {
+    console.log(`Studen: ${key}: ${student[key]}`);
+};
+const monthlyIncomes = {
+    salary: 1000,
+    bonus: 500,
+    sidehustle: '1000'
+};
+for (const stream in monthlyIncomes) {
+    console.log(`${stream}: ${monthlyIncomes[stream]}`);
+}
--- lesson 1/src/lesson5-8.ts
+++ lesson 1/src/lesson5-8.ts
@@ -28,5 +28,177 @@ const img = document.querySelector('img')!
 const myImg = document.getElementById('#id') as HTMLImageElement
 const nextImg = <HTMLImageElement>document.getElementById('#id')
 
-myImg.src
+//myImg.src
 
+
+
+//Classes
+
+class Coder {
+    secondLang!:string
+    constructor(
+        public name:string,
+        readonly music: string, 
+        private age: number, 
+        protected lang: string = 'Typescript'){
+        this.name = name,
+        this.music = music,
+        this.age = age,
+        this.lang = lang
+    }
+
+    public getAge() {
+        return `Hello, I'm ${this.age}`
+    }
+    
+}
+
+const Dave = new Coder('Santi', 'Electronic', 21, 'JavaSript')
+console.log(Dave.getAge)
+//Extends
+class WebDev extends Coder{
+    constructor(
+        public computer: string,
+        name: string,
+        music:string,
+        age:number,
+        lang:string = 'Typescript'
+    ){
+        super(name, music, age,lang)
+        this.computer = computer
+    }
+    public getLang(){
+        return `I wirte ${this.lang}`
+    }
+}
+
+const Mari = new WebDev('HP','Mari','POP',23,'Python')
+//Interface
+interface Musician{
+    name:string
+    instrument:string
+    play(action:string):string
+}
+
+class Percusionist implements Musician{
+    constructor(public name:string, public instrument:string) {
+        this.name = name
+        this. instrument = instrument
+    }
+    public play(action:string):string{
+        return `You are ${action} ${this.instrument}`
+    }
+}
+
+const Page = new Percusionist('Ana','Tambor')
+console.log(Page.play('Beating'))
+
+// Static variables and methods
+class Peep {
+    static count:number = 0
+
+    static getCount(): number{
+        return Peep.count
+    }
+    public id:number
+
+    constructor(public name:string) {
+        this.name= name
+        this.id = ++Peep.count
+    }
+}
+
+const John = new Peep('John')
+const Steve = new Peep('Steve')
+const Amy = new Peep ('Amy')
+
+console.log(Steve.id)
+
+// Setters and getters
+class Bands {
+    private dataState:string[]
+    constructor (){
+        this.dataState = []
+    }
+    public get data():string[]{
+        return this.dataState
+    }
+    public set data(value: string[]){
+        if(Array.isArray(value)&& value.every(el =>typeof el === 'string')){
+            this.dataState = value
+            return
+        }else throw new Error ('Param is not an array of strings')
+    }
+}
+
+const MyBands = new Bands()
+MyBands.data = ['Rock', 'Pop', 'Jazz']
+console.log(MyBands.data)
+MyBands.data = [...MyBands.data,'Metal']
+console.log(MyBands.data)
+//MyBands.data = ['Van Halen',2134]
+
+// Index Signatures
+
+interface TransacionObj {
+    readonly [index:string] : number
+    Pizza : number,
+    Books: number,
+    Job: number
+}
+
+const todaysTransactions : TransacionObj ={
+    Pizza: -10,
+    Books: 20,
+    Job: 1000
+}
+console.log(todaysTransactions.Pizza)
+
+const todayNetTransactions = (transactions : TransacionObj):number =>{
+    let total = 0
+    for (const transaction in transactions){
+        total += transactions[transaction]
+    }
+    return total
+}
+
+interface Student {
+    //[key:string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?:number[]
+}
+
+const student : Student ={
+    
+    name: 'John',
+    GPA: 3.5,
+    classes: [1,2,3,4,5]
+}
+
+//console.log(student.test)
+
+for (const key in student){
+    console.log(`${key}: ${student[key as keyof Student]}`)
+}
+
+Object.keys(student).map(key => {
+    console.log(student[key as keyof typeof student])
+})
+
+const stundentLog = (student:Student, key: keyof Student) =>{
+    console.log(`Studen: ${key}: ${student[key]}`)
+}
+
+// Another way to create interface and use index signatures
+type Streams = 'salary' |'bonus'|'sidehustle'
+type Incomes = Record<Streams, number |string >
+
+const monthlyIncomes : Incomes ={
+    salary: 1000,
+    bonus: 500,
+    sidehustle: '1000'
+}
+for (const stream in monthlyIncomes){
+    console.log(`${stream}: ${monthlyIncomes[stream as keyof Incomes]}`)
+}
