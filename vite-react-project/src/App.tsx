d155b3ce9a10c56dc42f2b1042475be263962e7a
SantiagoYpes <santiyezu8@hotmail.com>
2025-04-17 19:12:15 -0500

    Level 7-12

--- vite-react-project/src/App.tsx
+++ vite-react-project/src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Heading from "./components/Heading";
+import Section from "./components/Section";
+import Counter from "./components/Counter";
+import List from "./components/List";
+import { useState, useEffect, useCallback, useMemo, useRef } from "react";
+
+interface User{
+  id: number,
+  username : string
+}
+
+type fibFunc = (n:number) =>number
+
+const fib:fibFunc = (n) =>{
+  if(n<=2){
+    return n
+  }
+  return fib (n-1) + fib(n-2)
+}
+
+const myNum: number = 37
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number>(1)
+  const [users, setUsers] = useState<User[] | null >(null)
+
+  const inputRefer = useRef<HTMLInputElement>(null)
+  
+
+  useEffect(() => {
+    console.log('mounting')
+    console.log('Users:',users)
+    return () => {
+      console.log('unmounting')
+    }
+  }, [users])
+
+
+  const addTwo = useCallback(
+    () => {
+      console.log(inputRefer?.current?.value)
+      setCount(prev =>prev+2)
+    },
+    [],
+  )
+  
+  const result = useMemo(()=> fib(myNum),[myNum])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+
+    <h1>Other counter {count}</h1>
+    <button onClick={addTwo}></button>
+    <h1>{result}</h1>
+    <input ref={inputRefer} type="text" />
     </>
   )
 }
